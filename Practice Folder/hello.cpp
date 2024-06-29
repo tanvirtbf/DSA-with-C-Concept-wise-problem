8ccff273e2cde960ccdfb1773af881e711108c97
tanvirtbf <tanver018765@gmail.com>
2024-06-29 16:33:11 +0600

    solve star pattern complex

--- Practice Folder/hello.cpp
+++ Practice Folder/hello.cpp
@@ -2,16 +2,28 @@
 using namespace std;
 
 int main() {
-    int row,col;
+    /*int row,col;
     cin >> row >> col;
     for(int i = row; i>=1; i--){
         for(int j = 1; j<=col; j++){
             if(j>=i){
-                cout << "*" << " ";
-            }
-            else{
+                cout << "*";
+            }else{
                 cout << " ";
             }
+        }
+        cout << endl;
+    }*/
+    int row ,col;
+    cin >> row >> col;
+    for(int i = 1; i<=row; i++){
+        //space printing..
+        for(int j = 1; j<=col-i; j++){
+            cout << " ";
+        }
+        //star printing...
+        for(int j=1; j<=i; j++){
+            cout << "*";
         }
         cout << endl;
     }
