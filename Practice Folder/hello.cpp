9f4d58bc59cba559e23d68271ad1c4ec283dc4e5
tanvirtbf <tanver018765@gmail.com>
2024-06-29 16:53:54 +0600

    complex number and alphabatic printing problem solve

--- Practice Folder/hello.cpp
+++ Practice Folder/hello.cpp
@@ -2,28 +2,27 @@
 using namespace std;
 
 int main() {
-    /*int row,col;
-    cin >> row >> col;
-    for(int i = row; i>=1; i--){
-        for(int j = 1; j<=col; j++){
-            if(j>=i){
-                cout << "*";
-            }else{
-                cout << " ";
-            }
-        }
-        cout << endl;
-    }*/
-    int row ,col;
-    cin >> row >> col;
-    for(int i = 1; i<=row; i++){
-        //space printing..
+    int row=5,col=5;
+    for(int i=1; i<=row;i++){
+        //space printing...
         for(int j = 1; j<=col-i; j++){
             cout << " ";
         }
-        //star printing...
-        for(int j=1; j<=i; j++){
-            cout << "*";
+        //number printing...
+        for(int j = 1; j<=i; j++){
+            cout << j;
+        }
+        cout << endl;
+    }
+    int r=5,c=5;
+    for(int i = 1; i<=r; i++){
+        for(int j = 1; j<=c-i; j++){
+            cout << " ";
+        }
+        for(int j = 1; j<=i; j++){
+            char a = 'A';
+            a = a + j-1;
+            cout << a;
         }
         cout << endl;
     }
