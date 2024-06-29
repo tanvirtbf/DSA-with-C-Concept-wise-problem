7f5a313d99dafa9621cabc7752772d9dae475e08
tanvirtbf <tanver018765@gmail.com>
2024-06-29 15:40:04 +0600

    complex number pattern problem solved

--- Practice Folder/hello.cpp
+++ Practice Folder/hello.cpp
@@ -2,11 +2,18 @@
 using namespace std;
 
 int main() {
-    int row,col;
-    cin >> row >> col;
+    int row=5,col=5;
     for(int i = 1; i<=row; i++){
-        for(int j = i; j>=1; j--){
-            cout << j<< " " ;
+        for(int j = 1; j<=col-i+1; j++){
+            cout << j << " " ;
+        }
+        cout << endl;
+    }
+    //reverse
+    int rows=5,cols=5;
+    for(int i = 0; i<rows; i++){
+        for(int j = 0; j<=i; j++){
+            cout << cols - j << " " ;
         }
         cout << endl;
     }
