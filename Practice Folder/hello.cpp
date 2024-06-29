46e1c82daf444330d25eb92f8f80b960fede7eca
tanvirtbf <tanver018765@gmail.com>
2024-06-29 10:39:19 +0600

    calculate power of number problem solved

--- Practice Folder/hello.cpp
+++ Practice Folder/hello.cpp
@@ -3,10 +3,19 @@
 using namespace std;
 
 int main() {
-    int n , i ;
+    /*int n , i ;
     cout << "please enter n " << endl;
     cin >> n;
     for(i = 1; i<=10; i++){
         cout << endl << i*n << endl;
+    }*/
+    //n = 2; p = 3 ; result = 2 power 3 = 2 * 2 * 2 * 2 * 2
+
+    int n, p , result;
+    cin >> n >> p;
+    result = n;
+    for(int i = 1; i<p; i++){
+        result *= n;
     }
+    cout << result << endl;
 }
