f0f85beb39e0f4f1261eda363427b3ed24635034
tanvirtbf <tanver018765@gmail.com>
2024-06-29 10:11:44 +0600

    another problem solved

--- Practice Folder/hello.cpp
+++ Practice Folder/hello.cpp
@@ -1,9 +1,9 @@
-//print numbers from 1 to 100 but difference is 3
+//print multilplication table making
 #include <iostream>
 using namespace std;
 
 int main() {
-    for(int i = 1; i<=100; i+=3){
-        cout << i << endl;
+    for(int i = 1; i <=10; i++){
+        cout << 6 * i << endl;
     }
 }
