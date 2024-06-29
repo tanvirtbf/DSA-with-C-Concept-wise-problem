860d64257e1961290d68da93edaacedac20b728b
tanvirtbf <tanver018765@gmail.com>
2024-06-29 11:08:29 +0600

    sum of n natural number problem solved

--- Practice Folder/hello.cpp
+++ Practice Folder/hello.cpp
@@ -1,4 +1,4 @@
-//print multilplication table making
+//calculate power of number
 #include <iostream>
 using namespace std;
 
@@ -8,7 +8,7 @@ int main() {
     cin >> n;
     for(i = 1; i<=10; i++){
         cout << endl << i*n << endl;
-    }*/
+    }
     //n = 2; p = 3 ; result = 2 power 3 = 2 * 2 * 2 * 2 * 2
 
     int n, p , result;
@@ -18,4 +18,28 @@ int main() {
         result *= n;
     }
     cout << result << endl;
+
+    int n, pow, r, i;
+    cout << "please enter a number : " << endl;
+    cin >> n ;
+    cout << "please enter a power : " << endl;
+    cin >> pow;
+    r = n;
+    for(i = 1; i<pow; i++){
+        r = r * n;
+    }
+    cout << r << endl;
+    // sum of n Natural number
+    int n , result=0;
+    cout << "please enter a number : " << endl;
+    cin >> n;
+    for(int i = 1; i <=n; i++){
+        result = result +  i;
+    }
+    cout << result << endl;*/
+    // another technic
+    int n ;
+    cin >> n;
+    cout << (n * (n+1)) / 2;
+
 }
