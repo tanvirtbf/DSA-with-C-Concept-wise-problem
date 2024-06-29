58724db1df5c94dfcc3180bd70288608e95b7195
tanvirtbf <tanver018765@gmail.com>
2024-06-29 14:17:46 +0600

    number pattern 2 solved

--- Practice Folder/hello.cpp
+++ Practice Folder/hello.cpp
@@ -1,13 +1,12 @@
-//print n number factorial value
 #include <iostream>
 using namespace std;
 
 int main() {
     int row,col;
     cin >> row >> col;
-    for(int i = 1; i<=row; i++){
-        for(int j = 1; j<=col; j++){
-            cout << i << " ";
+    for(int i=1; i<=row; i++){
+        for(int j=1; j<=col; j++){
+            cout << j << " ";
         }
         cout << endl;
     }
