d8f94a4b123ce09300d8c4e636936614229d20ab
tanvirtbf <tanver018765@gmail.com>
2024-06-29 11:34:47 +0600

    Prime number problem solved

--- Practice Folder/hello.cpp
+++ Practice Folder/hello.cpp
@@ -1,13 +1,23 @@
-//calculate power of number
+//print n number factorial value
 #include <iostream>
 using namespace std;
 
 int main() {
-    int n,result=1;
+    //prime number
+    string yesPrime = "This is a prime Number", noPrime="This is not a prime number";
+    int n, check=0;
     cin >> n;
-    for(int i = 1; i<=n; i++){
-        result = result * i;
+    if(n<2){
+        cout << noPrime << endl;
+        return 0;
     }
-    cout << result << endl;
-
+    for(int i = 2; i<n; i++ ){
+        if(n%i == 0){
+            check++;
+            cout << noPrime << endl;
+            return 0;
+        }
+    }
+    cout << yesPrime << endl;
+    return 0;
 }
