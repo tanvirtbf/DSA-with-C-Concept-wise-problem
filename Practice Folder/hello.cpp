1a0bf69cf0cea9fc832554b4e9a105b3e695e19d
tanvirtbf <tanver018765@gmail.com>
2024-06-29 16:18:44 +0600

    the triangle pattern problem solve

--- Practice Folder/hello.cpp
+++ Practice Folder/hello.cpp
@@ -2,18 +2,16 @@
 using namespace std;
 
 int main() {
-    int row=5,col=5;
-    for(int i = 1; i<=row; i++){
-        for(int j = 1; j<=col-i+1; j++){
-            cout << j << " " ;
-        }
-        cout << endl;
-    }
-    //reverse
-    int rows=5,cols=5;
-    for(int i = 0; i<rows; i++){
-        for(int j = 0; j<=i; j++){
-            cout << cols - j << " " ;
+    int row,col;
+    cin >> row >> col;
+    for(int i = row; i>=1; i--){
+        for(int j = 1; j<=col; j++){
+            if(j>=i){
+                cout << "*" << " ";
+            }
+            else{
+                cout << " ";
+            }
         }
         cout << endl;
     }
