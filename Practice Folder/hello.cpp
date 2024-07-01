b030f6ee3a53e098e262409fcc5a037c3ef51e7f
tanvirtbf <tanver018765@gmail.com>
2024-07-01 15:14:45 +0600

    start this folder named array simple problem

--- Practice Folder/hello.cpp
+++ Practice Folder/hello.cpp
@@ -2,18 +2,22 @@
 using namespace std;
 
 int main() {
-    int n;
-    cout << "Please Enter your array size : ";
-    cin >> n;
-    int arr[n];
-    for(int i = 0; i<n; i++){
+    int arr[10];
+    int a[100],b[10];
+    for(int i = 0; i<10; i++){
         cin >> arr[i];
-    }
-    int result = arr[0];
-    for(int i = 0; i<n; i++){
-        if(result<arr[i]){
-            result = arr[i];
+        if(i>0 && arr[i]<arr[i-1]){
+            cout << "Please Enter a Non-decreasing number : " << endl;
+            arr[i] = 0;
+            cin >> arr[i];
         }
     }
-    cout << result << endl;
+    for(int i = 0; i<100; i++){
+        if(arr[i] == i){
+            a[i] = arr[i];
+        }
+    }
+    for(int i = 0; i<100; i++){
+        cout << a[i];
+    }
 }
