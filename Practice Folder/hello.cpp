a13d2b4edba8000648e53f7aa13b022566ab2cf8
tanvirtbf <tanver018765@gmail.com>
2024-07-01 10:48:59 +0600

    triangle problem solved

--- Practice Folder/hello.cpp
+++ Practice Folder/hello.cpp
@@ -7,8 +7,13 @@ int main() {
     col = row * 2 - 1;
     int left = row, right = row;
     for(int i = row ; i>=1; i--){
-        for(int j = left; j<=right; j++){
-            cout << "*";
+        for(int j = 1; j<=col; j++){
+            if(j>=left && j<=right){
+                cout << "*";
+            }
+            else{
+                cout << " ";
+            }
         }
         left = left -1;
         right = right +1;
