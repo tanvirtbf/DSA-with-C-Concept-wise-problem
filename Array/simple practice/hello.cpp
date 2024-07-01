2006e0a5b8fdda050afb92006d08b8acae2d4d79
tanvirtbf <tanver018765@gmail.com>
2024-07-01 15:20:51 +0600

    Search an Element of an array (return index number)

--- Array/simple practice/hello.cpp
+++ Array/simple practice/hello.cpp
@@ -1,6 +1,21 @@
+//Search an Element of an array (return index number)
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-    cout << "Hello World@";
+    int n ;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+    cout << "Now give a number for searching from this array : " << endl;
+    int searchNum ;
+    cin >> searchNum;
+    for(int i = 0; i<n; i++){
+        if(searchNum == arr[i]){
+            cout << searchNum << " number index is : " << i << endl;
+            return 0;
+        }
+    }
 }
