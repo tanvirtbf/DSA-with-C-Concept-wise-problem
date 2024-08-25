5be18b4a59b112020460946d3a59762a6b5c03ec
tanvirtbf <tanver018765@gmail.com>
2024-08-25 18:23:16 +0600

    start again array

--- Array/simple practice/hello.cpp
+++ Array/simple practice/hello.cpp
@@ -3,18 +3,45 @@
 using namespace std;
 
 int main(){
-
-    int n,findIndex;
-    cout << "Enter Array Size Number : " ;
-    cin >> n;
-    cout << endl << "Please enter search number : " << endl;
-    cin >> findIndex;
-    int arr[n];
-    for(int i = 0; i<n; i++){
-        cin >> arr[i];
-        if(findIndex == arr[i]){
-            cout << "Search Number Index is : " << i << endl;
-            return 0;
-        }
+    // 5 Approach for create array
+    
+    /*
+    // first approach :
+    int arr[5] = {1,2,3,4,5};
+    for(int i = 0; i<5; i++){
+        cout << arr[i] << endl;
     }
+    
+    // second approach
+    int arr2[] = {1,2,3,4,5};
+    for(int i = 0; i<5; i++){
+        cout << arr2[i] << endl;
+    }
+    
+    // third approach
+    int arr3[5];
+    for(int i = 0; i<5; i++){
+        cin >> arr3[i];
+    }
+    for(int i = 0; i<5; i++){
+        cout << arr3[i] << endl;
+    }
+    */
+    
+    // fourth approach
+    int arr4[5] = {1,2};
+    for(int i = 0; i<5; i++){
+        cout << arr4[i] << endl;
+    } // 1 2 0 0 0 
+    
+    // fifth approach
+    int arr5[5] = {0};
+    for(int i = 0; i<5; i++){
+        cout << arr5[i] << endl;
+    } // 0 0 0 0 0
+    int arr55[5] = {1};
+    for(int i = 0; i<5; i++){
+        cout << arr55[i] << endl;
+    }
+
 }
