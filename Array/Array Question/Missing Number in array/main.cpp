9485bc0cc2228ced25658a51ed032da819162bc4
tanvirtbf <tanver018765@gmail.com>
2024-08-26 14:17:47 +0600

    second approach of missing number

--- Array/Array Question/Missing Number in array/main.cpp
+++ Array/Array Question/Missing Number in array/main.cpp
@@ -3,6 +3,51 @@ using namespace std;
 
 int main(){
     
+    // Second Approach
+    int preSum = 0 , afterSum = 0;
+    int n;
+    cin >> n;
+    int arr[n-1];
+    for(int i=0; i<n-1; i++){
+        cin >> arr[i];
+        preSum += arr[i];
+    }
+    for(int i = 1; i<=n; i++){
+        afterSum += i;
+    }
+    // Or 
+    int afterSum2 = (n * (n+1)) / 2;
+    int result = afterSum2 - preSum;
+    cout << result;
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
     // First Approach
     int N;
     cin >> N;
@@ -16,8 +61,5 @@ int main(){
             return 0;
         }
     }
-}
 
-
-
-
+*/
