c2231e7bd2dd571611c425fee08145c5a0bf42ee
tanvirtbf <tanver018765@gmail.com>
2024-08-26 12:33:55 +0600

    second max first approach|

--- Array/Array Question/find second maximum number/main.cpp
+++ Array/Array Question/find second maximum number/main.cpp
@@ -4,12 +4,25 @@ using namespace std;
 int main(){
     
     // First Approach
+    int maxNum = INT_MIN;
     int n;
     cin >> n;
     int arr[n];
+    int temp[n];
     for(int i = 0; i<n; i++){
         cin >> arr[i];
+        if(maxNum<arr[i]){
+            maxNum = arr[i];
+        }
     }
+    int result = INT_MIN;
+    for(int i=0; i<n; i++){
+        if(arr[i]!=maxNum && result<arr[i]){
+            result = arr[i];
+        }
+    }
+    cout << "Second Max Number is : " << result;
+    
     
 
 }
