83f217a8f2fa630fd1cb2c693f59ac041b3557ca
tanvirtbf <tanver018765@gmail.com>
2024-08-27 14:29:38 +0600

    update

--- Array/Sorting/Selection Sort/main.cpp
+++ Array/Sorting/Selection Sort/main.cpp
@@ -2,20 +2,21 @@
 using namespace std;
 
 int main(){
-    int temp[5];
-    int arr[5] = {3,4,2,1,5};
-    for(int i = 0; i<5; i++){
-        int var = INT_MAX;
-        for(int j = i; j<5; j++){
-            if(arr[j]<var){
-                var = arr[j];
-                temp[j] = var;
+    int n=5, result = INT_MAX;
+    int arr[n] = {2,5,3,1,4};
+    
+    for(int i = 0; i<n; i++){ // i = 1
+        int temp = arr[i]; // 5
+        for(int j = i; j<n; j++){ // j = 1
+            if(arr[j]<arr[i]){
+                result = arr[j]; // 3
+                arr[j] = temp;
+                arr[i] = result; // 3
             }
         }
-
+        result = arr[i]; // 1
     }
-
-    for(int i=0; i<5; i++){
-        cout << temp[i];
+    for(int i = 0 ; i<n; i++){
+        cout << arr[i];
     }
 }
