17c2b57d3978ef8b2b05067ecf143dc920bc2287
tanvirtbf <tanver018765@gmail.com>
2024-08-28 10:26:25 +0600

    bubble sort solve by alone

--- Array/Sorting/Bubble Sort/main.cpp
+++ Array/Sorting/Bubble Sort/main.cpp
@@ -3,16 +3,14 @@ using namespace std;
 
 int main(){
     int n = 5;
-    int arr[n] = {4,1,3,5,2};
+    int arr[n] = {4,11,30,5,2};
     
-    for(int i = 0; i<n; i++){
-        int index = i;
-        for(int j = i; j<n; j++){
-            if(arr[index]<arr[j]){
-                index = j;
+    for(int i = 1; i<n; i++){
+        for(int j = 1; j<=n-i; j++){
+            if(arr[j]<arr[j-1]){
+                swap(arr[j],arr[j-1]);
             }
         }
-        swap(arr[i],arr[index]);
     }
     
     for(int i = 0; i<n; i++){
