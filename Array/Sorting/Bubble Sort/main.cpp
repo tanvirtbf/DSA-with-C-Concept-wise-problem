0e523daec5cc94429149e77a5e2a963914cac6e2
tanvirtbf <tanver018765@gmail.com>
2024-08-28 11:58:33 +0600

    Bubble sort descending order

--- Array/Sorting/Bubble Sort/main.cpp
+++ Array/Sorting/Bubble Sort/main.cpp
@@ -2,18 +2,16 @@
 using namespace std;
 
 int main(){
-    int n = 5;
-    int arr[n] = {4,11,30,5,2};
-    
+    int n = 5; 
+    int arr[n] = {3,1,4,2,0};
     for(int i = n-2; i>=0; i--){
         for(int j = 0; j<=i; j++){
-            if(arr[j]>arr[j+1]){
+            if(arr[j]<arr[j+1]){
                 swap(arr[j],arr[j+1]);
             }
         }
     }
-    
-    for(int i = 0; i<n; i++){
+    for(int i=0; i<n; i++){
         cout << arr[i] << " ";
     }
 }
@@ -42,46 +40,36 @@ int main(){
 // Ascending order
 /*
 
-    int n=5;
-    int arr[n] = {2,5,3,1,4};
+    int n = 5;
+    int arr[n] = {4,11,30,5,2};
     
-    for(int i=0; i<n; i++){
-        int index = i;
-        for(int j = i; j<n; j++){
-            if(arr[index]>arr[j]){
-                index = j;
+    for(int i = n-2; i>=0; i--){
+        for(int j = 0; j<=i; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
             }
         }
-        int temp = arr[i];
-        arr[i] = arr[index];
-        arr[index] = temp;
-        // swap(arr[i],arr[index]);
     }
     
-    for(int i = 0 ; i<n; i++){
-        cout << arr[i];
+    for(int i = 0; i<n; i++){
+        cout << arr[i] << " ";
     }
     
     
     
     
     // Descending Order
-    int n=5;
-    int arr[n] = {2,5,3,1,4};
-    
-    for(int i = 0; i<n; i++){
-        int index = i;
-        for(int j = i; j<n; j++){
-            if(arr[index]<arr[j]){
-                index = j;
+    int n = 5; 
+    int arr[n] = {3,1,4,2,0};
+    for(int i = n-2; i>=0; i--){
+        for(int j = 0; j<=i; j++){
+            if(arr[j]<arr[j+1]){
+                swap(arr[j],arr[j+1]);
             }
         }
-        int temp = arr[index];
-        arr[index] = arr[i];
-        arr[i] = temp;
     }
-    for(int i = 0 ; i<n; i++){
-        cout << arr[i];
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
     }
 
 */
